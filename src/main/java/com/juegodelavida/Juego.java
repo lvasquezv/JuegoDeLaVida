789ccbff5d81a77ff71f7dc6033e37be8f6d6539
Luis Rolando Vasquez Velasquez <vasquez.luisrolando@gmail.com>
2019-08-18 10:58:25 -0400

    R4_CM_conTresVecinasVivas_Vive : REFACTORIZACION Se quito el metodo “tieneVecinasVivas” que fue reemplazado por el metodo “cantidadVecinasVivas”, y ademas que se modifico un poco la forma de hacer la validacion en el metodo “obtenerEstadoSiguiente”. Despues probando nuevamente todo esta OK:

--- src/main/java/com/juegodelavida/Juego.java
+++ src/main/java/com/juegodelavida/Juego.java
@@ -22,31 +22,22 @@ public class Juego {
     public static boolean obtenerEstadoSiguiente(boolean[][] tablero, int f, int c)
     {
         boolean cv = tablero[f][c];
-        if (cv){
-            if (cantidadVecinasVivas(tablero, f, c)<2 || cantidadVecinasVivas(tablero,f,c)> 3)
-                return false;
-            else
-                return true;
-        }else{
-            if (cantidadVecinasVivas(tablero, f, c) ==3)
-                return true;
-            else
-                return false;
-        }
-
-    }
-    public static int cantidadVecinasVivas(boolean[][] tablero, int f, int c){
-        int cantidad = 0;
-        if (tablero[f-1][c-1]) cantidad++; if (tablero[f-1][c]) cantidad++; if (tablero[f-1][c+1]) cantidad++;
-        if (tablero[f][c-1]) cantidad++; if (tablero[f][c+1]) cantidad++; if (tablero[f+1][c-1]) cantidad++;
-        if (tablero[f+1][c]) cantidad++; if (tablero[f+1][c+1]) cantidad++;
-        return cantidad;
-    }
-    public static boolean tieneVecinasVivas(boolean[][] tablero, int f, int c){
-        if (!tablero[f-1][c-1] && !tablero[f-1][c] && !tablero[f-1][c+1] && !tablero[f][c-1]
-                && !tablero[f][c+1] && !tablero[f+1][c-1] && !tablero[f+1][c] && !tablero[f+1][c+1] )
+        if ((cv &&  (cantidadVecinasVivas(tablero, f, c)<2 || cantidadVecinasVivas(tablero,f,c)> 3)) ||
+                (!cv && (cantidadVecinasVivas(tablero, f, c) !=3)))
             return false;
         else
             return true;
+    }
+    public static int cantidadVecinasVivas(boolean[][] tablero, int f, int c) {
+        int cantidad = 0;
+        if (tablero[f - 1][c - 1]) cantidad++;
+        if (tablero[f - 1][c]) cantidad++;
+        if (tablero[f - 1][c + 1]) cantidad++;
+        if (tablero[f][c - 1]) cantidad++;
+        if (tablero[f][c + 1]) cantidad++;
+        if (tablero[f + 1][c - 1]) cantidad++;
+        if (tablero[f + 1][c]) cantidad++;
+        if (tablero[f + 1][c + 1]) cantidad++;
+        return cantidad;
     }
 }
