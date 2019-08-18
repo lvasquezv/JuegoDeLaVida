8083e6a885ddd9f49539a92107b46f062a8e734d
Luis Rolando Vasquez Velasquez <vasquez.luisrolando@gmail.com>
2019-08-17 23:31:37 -0400

    R2_CV_conDosTresVecinasVivas_Vive : ERROR

--- src/test/java/com/juegodelavida/JuegoTest.java
+++ src/test/java/com/juegodelavida/JuegoTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class JuegoTest{
+
     @Test
     public void R1_CV_menosDeDosVecinasVivas_Muere() {
         boolean[][] tablero =
@@ -21,5 +22,23 @@ public class JuegoTest{
                 };
         boolean valor = Juego.obtenerEstadoSiguiente(tablero,2,4);
         Assert.assertFalse(valor);
+    }
+    @Test
+    public void R2_CV_conDosTresVecinasVivas_Vive() {
+        boolean[][] tablero =
+                {
+                        {false, false, false, false, false}
+                        , {false, false, false, false, false}
+                        , {false, false, false, false, false}
+                        , {false, false, true, false, false}
+                        , {false, false, true, false, false}
+                        , {false, false, true, false, false}
+                        , {false, false, false, false, false}
+                        , {false, false, false, false, false}
+                        , {false, false, false, false, false}
+                        , {false, false, false, false, false}
+                };
+        boolean valorActual = Juego.obtenerEstadoSiguiente(tablero, 2, 4);
+        Assert.assertTrue(valorActual);
     }
 }
