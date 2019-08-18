83927f1b5429ca9f9849ca5119f6c1a65e2efbee
Luis Rolando Vasquez Velasquez <vasquez.luisrolando@gmail.com>
2019-08-18 10:14:58 -0400

    R2_CV_conDosTresVecinasVivas_Vive : REFACTORIZACION Agregando el metodo tieneVecinasVivas, el cual me indica si una celda tiene alguna celda vecina viva.

--- src/main/java/com/juegodelavida/Juego.java
+++ src/main/java/com/juegodelavida/Juego.java
@@ -19,10 +19,16 @@ public class Juego {
         return tableroEsperado;
     }
 
-    public static boolean obtenerEstadoSiguiente(boolean[][] tablero, int i, int i1)
+    public static boolean obtenerEstadoSiguiente(boolean[][] tablero, int f, int c)
     {
-        if (!tablero[i-1][i1-1] && !tablero[i-1][i1] && !tablero[i-1][i1+1] && !tablero[i][i1-1]
-                && !tablero[i][i1+1] && !tablero[i+1][i1-1] && !tablero[i+1][i1] && !tablero[i+1][i1+1] )
+        if (!tieneVecinasVivas(tablero, f, c) )
+            return false;
+        else
+            return true;
+    }
+    public static boolean tieneVecinasVivas(boolean[][] tablero, int f, int c){
+        if (!tablero[f-1][c-1] && !tablero[f-1][c] && !tablero[f-1][c+1] && !tablero[f][c-1]
+                && !tablero[f][c+1] && !tablero[f+1][c-1] && !tablero[f+1][c] && !tablero[f+1][c+1] )
             return false;
         else
             return true;
